987a09e8b5bc631c3afc64ca15214c087d3eac83
froncu <jakubfratczak204@gmail.com>
2026-08-23 19:51:13 +0200

    [FEATURE] make Tephra fetch the registry scripts

--- ports/tephra/portfile.cmake
+++ ports/tephra/portfile.cmake
@@ -1,7 +1,15 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/froncu/tephra.git
-    REF 926e4f1446d68740dc01f3a008263502a3c2492d)
+    REF 69bd7a56e7b97790c30b08598fa18cca3479cd66)
+
+vcpkg_from_git(
+   OUT_SOURCE_PATH REGISTRY_SOURCE_PATH
+   URL https://github.com/froncu/registry.git
+   REF a51306fb8ee7aaa320570600904a16a950baacdd)
+
+file(MAKE_DIRECTORY ${SOURCE_PATH}/registry/scripts)
+file(COPY ${REGISTRY_SOURCE_PATH}/scripts/target_defaults.cmake DESTINATION ${SOURCE_PATH}/registry/scripts)
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
