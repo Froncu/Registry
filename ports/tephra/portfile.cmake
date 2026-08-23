e2208b8f8f374dba521b3081b1385d9d18d17da3
froncu <jakubfratczak204@gmail.com>
2026-08-23 20:09:45 +0200

    [FEATURE] update Tephra's portfile

--- ports/tephra/portfile.cmake
+++ ports/tephra/portfile.cmake
@@ -1,16 +1,9 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/froncu/tephra.git
-    REF 69bd7a56e7b97790c30b08598fa18cca3479cd66)
-
-vcpkg_from_git(
-   OUT_SOURCE_PATH REGISTRY_SOURCE_PATH
-   URL https://github.com/froncu/registry.git
-   REF a51306fb8ee7aaa320570600904a16a950baacdd)
-
-file(MAKE_DIRECTORY ${SOURCE_PATH}/registry/scripts)
-file(COPY ${REGISTRY_SOURCE_PATH}/scripts/target_defaults.cmake DESTINATION ${SOURCE_PATH}/registry/scripts)
+    REF b152c8c188e3acb36cec0016cb498a411d015ae9)
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME tephra)
 vcpkg_copy_tools(TOOL_NAMES tephra AUTO_CLEAN)
