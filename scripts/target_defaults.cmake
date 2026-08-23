144f4cdbb46e5569ab709e0ddcb162c77cafdb5e
froncu <jakubfratczak204@gmail.com>
2026-08-23 22:13:35 +0200

    [BUILD] update target defaults script

--- scripts/target_defaults.cmake
+++ scripts/target_defaults.cmake
@@ -3,7 +3,8 @@ function(set_target_defaults TARGET_NAME)
       CXX_STANDARD 23
       CXX_STANDARD_REQUIRED TRUE)
 
-   target_compile_options(${PROJECT_NAME}
-      PRIVATE $<$<CXX_COMPILER_FRONTEND_VARIANT:GNU>:-Wall;-Wextra;-Wpedantic;-Werror;-Wno-braced-scalar-init>
-      PRIVATE $<$<CXX_COMPILER_FRONTEND_VARIANT:MSVC>:/W4;/WX>)
+   target_compile_options(${TARGET_NAME}
+      PRIVATE $<$<CXX_COMPILER_FRONTEND_VARIANT:GNU>:-Wall;-Wextra;-Wpedantic;-Werror>
+      PRIVATE $<$<CXX_COMPILER_FRONTEND_VARIANT:MSVC>:/W4;/WX>
+      PRIVATE $<$<CXX_COMPILER_ID:Clang>:-Wno-braced-scalar-init;-Wno-unused-template>)
 endfunction()
